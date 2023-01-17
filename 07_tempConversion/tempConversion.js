3b5e6e19f27253a34c3dcf88b64c5f036e0e633c
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-17 14:13:18 +0530

    Finish tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit-32)*5.0/9;
+  celsius = Math.round(celsius * 10)/10.0;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = 9.0*celsius/5.0 + 32;
+  fahrenheit = Math.round(fahrenheit*10)/10.0;
+  return fahrenheit;
 };
 
 // Do not edit below this line
