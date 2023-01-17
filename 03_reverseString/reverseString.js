cc569fe5ce581535758a61ad3347e3bd950f1020
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-17 08:57:00 +0530

    Finish reversestring exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const splitString = string.split("");
+    let reversed = "";
+    for (let i = splitString.length -1; i >= 0; i--) {
+        reversed += splitString[i];
+    }
+    return reversed;
 
 };
 
