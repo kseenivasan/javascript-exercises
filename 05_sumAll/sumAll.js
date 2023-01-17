33964a265a8d2cf3dd3f005ed63cd32d248a6e5b
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-17 13:59:18 +0530

    Finish sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let sum = 0;
+    let start;
+    let end;
+    if (first < 0 || last < 0 || typeof first !== "number" || typeof last !== "number") {
+        return "ERROR";
+    }
+    if (first < last) {
+        start = first;
+        end = last;
+    }
+    else {
+        start = last;
+        end = first;
+    }
+    for (let i = start; i <= end;i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
