7f383b6c69ee8d878167d7968e8e4be8958a4495
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-17 08:50:58 +0530

    Finish repeatstring exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+    let i = 0;
+    let returnString = "";
+    while (i < count) {
+        returnString += string;
+        i++;
+    }
+    if (count < 0) {
+        return "ERROR";
+    }
+    else {
+        return returnString;
+    }
 
 };
 
