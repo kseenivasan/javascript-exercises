5fc0e6e49b2280a518dbbac11f74da3ea9d68a20
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-22 13:29:53 +0530

    Finish 11 getthetitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(objArr) {
+    let arr = [];
+    objArr.forEach(element => {
+        arr.push(element.title);
+    });
+
+    return arr;
 
 };
 
