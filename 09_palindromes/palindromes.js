53a79427eb835ed5717e34eedb61e8476d936dcd
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-22 13:12:27 +0530

    Finish 09 palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (pattern) {
+    let punctuationless = pattern.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    let lower = punctuationless.toLowerCase();
+    let spaceless = lower.replaceAll(" ", "");
+    let i = 0;
+    let j = spaceless.length - 1;
+    while (i < j) {
+        if (spaceless.charAt(i) !== spaceless.charAt(j)) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
