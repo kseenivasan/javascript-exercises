f0123779fad081b35afeeead9a7deb26aeb45ace
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-22 13:23:37 +0530

    Finish 10 fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    let fibArray = [1,1];
+    while (fibArray.length < num) {
+        fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length -2]);
+    }
+
+    return fibArray[fibArray.length -1];
 
 };
 
